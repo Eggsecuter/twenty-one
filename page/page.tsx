8dd32ab6a8ada8a7354314e518fc1044560ffef2
Eggsecuter <adrian.habeler@gmail.com>
2024-04-22 14:36:53 +0200

    Add brand navigation

--- page/page.tsx
+++ page/page.tsx
@@ -4,7 +4,7 @@ export class PageComponent extends Component {
 	render(child) {
 		return <ui-page>
 			<ui-navigation>
-				<ui-brand>RE7 Card Game</ui-brand>
+				<ui-brand ui-href=''>RE7 Card Game</ui-brand>
 			</ui-navigation>
 
 			{child}
